c50d57de5f158998c100419b11548465328b8331
TenTraicion <tentraicion@gmail.com>
2023-05-08 00:08:20 +0600

    D085 S32 L559

--- models/product.model.js
+++ models/product.model.js
@@ -8,8 +8,20 @@ class Product {
     this.description = productData.description;
     this.image = productData.image;
     this.imagePath = `product-data/images/${productData.image}`;
-    this.imageURL = `/products/assets/images/${productData.image}`;
+    this.imageUrl = `/products/assets/images/${productData.image}`;
+    if (productData._id) {
+      this.id = productData._id.toString();
+    }
   }
+
+  static async findAll() {
+    const products = await db.getDb().collection('products').find().toArray();
+
+    return products.map(function(productDocument) {
+      return new Product(productDocument);
+    });
+  }
+
   async save() {
     const productData = {
       title: this.title,
