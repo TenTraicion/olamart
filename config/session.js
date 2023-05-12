621c34c8aa4d5739d048ac19ca994608f5a0c5e7
TenTraicion <tentraicion@gmail.com>
2023-05-12 20:06:39 +0600

    basic retouch

--- config/session.js
+++ config/session.js
@@ -2,16 +2,21 @@ const expressSession = require('express-session');
 const mongoDbStore = require('connect-mongodb-session');
 
 let mongoUrl = "mongodb://127.0.0.1:27017/";
-const db = process.env.MONGO_DB;
+let response = "sessions saved in local directory";
+let db = "online-shop";
 
 if (process.env.MONGODB_URL) {
 	mongoUrl = process.env.MONGODB_URL;
+	response = "sessions saved in mongodb url";
+	db = process.env.MONGO_DB;
 }
 if (process.env.MONGO_USER) {
 	const user = process.env.MONGO_USER;
 	const pwd = process.env.MONGO_PWD;
 	const cluster = process.env.MONGO_CLUSTER;
-	mongoUrl = `mongodb+srv://${user}:${pwd}@${cluster}.mongodb.net/`;
+	mongoUrl = `mongodb+srv://${user}:${pwd}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+	response = "sessions saved in mongodb cluster";
+	db = process.env.MONGO_DB;
 }
 
 function createSessionStore() {
@@ -22,7 +27,8 @@ function createSessionStore() {
     databaseName: db,
     collection: 'sessions'
   });
-
+  console.log(response);
+	console.log(`session: ${db}`);
   return store;
 }
 
--- data/database.js
+++ data/database.js
@@ -4,21 +4,28 @@ const MongoClient = mongodb.MongoClient;
 
 let database;
 let mongoUrl = 'mongodb://127.0.0.1:27017/';
-const db = process.env.MONGO_DB;
+let response = "connected to local directory"
+let db = "online-shop";
 
 if (process.env.MONGODB_URL) {
   mongoUrl = process.env.MONGODB_URL;
+  response = "connected to mongodb url"
+  db = process.env.MONGO_DB;
 }
 if (process.env.MONGO_USER) {
   const user = process.env.MONGO_USER;
   const pwd = process.env.MONGO_PWD;
   const cluster = process.env.MONGO_CLUSTER;
-  mongoUrl = `mongodb+srv://${user}:${pwd}@${cluster}.mongodb.net/`;
+  mongoUrl = `mongodb+srv://${user}:${pwd}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+  response = "connected to mongodb cluster"
+  db = process.env.MONGO_DB;
 }
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(mongoUrl);
   database = client.db(db);
+  console.log(response);
+  console.log(`server: ${db}`);
 }
 
 function getDb() {
